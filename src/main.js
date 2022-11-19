1816e0901844cb26d9effa541b8caff67070c157
Mahmudun Nabi Kajal <mahmudunnabikajal@gmail.com>
2022-11-19 21:38:26 +0600

    netlify.toml add

--- src/main.js
+++ src/main.js
@@ -2,14 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 
 new Vue({
   router,
   store,
-  VueSweetalert2,
   render: h => h(App)
 }).$mount('#app')
